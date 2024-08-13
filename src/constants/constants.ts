d31f3f70d4ca7f9a7921a394054ee85cf80a9db0
John Smith <fahmi.perdana00@gmail.com>
2024-08-13 23:42:46 +0700

    Initial commit

---
+++ src/constants/constants.ts
@@ -0,0 +1,7 @@
+// constants.ts
+export const CONSTANTS = {
+    AUTHOR: '@fahmiprdn16',
+    AUTHOR_GITHUB_LINK: 'https://github.com/author',
+    AUTHOR_INSTAGRAM_LINK: 'https://instagram.com/fahmiprdn16', // Tambahkan baris ini
+  };
+  
---
+++ src/data/songData.ts
@@ -0,0 +1,134 @@
+export const chill = [
+  {
+    id: 0,
+    name: 'Dewa 19 - Kangen',
+    mood: 'chill',
+    src: './assets/lofi/lagu1.mp3',
+  },
+  {
+    id: 1,
+    name: 'Skinyfabs - Happy',
+    mood: 'chill',
+    src: './assets/lofi/lagu2.mp3',
+  },
+  {
+    id: 2,
+    name: 'Budi Doremi - Melukis Senja',
+    mood: 'chill',
+    src: './assets/lofi/lagu3.mp3',
+  },
+  {
+    id: 3,
+    name: 'Pamungkas - To The Bone',
+    mood: 'chill',
+    src: './assets/lofi/lagu4.mp3',
+  },
+  {
+  id: 4,
+    name: 'Nadhif Basalamah - Penjaga Hati',
+    mood: 'chill',
+    src: './assets/lofi/lagu5.mp3',
+  },
+  {
+    id: 5,
+      name: 'Chrisye - Pergilah Kasih',
+      mood: 'chill',
+      src: './assets/lofi/lagu6.mp3',
+    },
+    {
+      id: 6,
+        name: 'Vierra - Seandainya',
+        mood: 'chill',
+        src: './assets/lofi/lagu7.mp3',
+      },
+];
+
+export const jazzy = [
+  {
+    id: 0,
+    name: 'Ghea Indrawari - Jiwa Yang Bersedih',
+    mood: 'jazzy',
+    src: './assets/lofi/lagujazz1.mp3',
+  },
+  {
+    id: 1,
+    name: 'RAN - Pandangan Pertama',
+    mood: 'jazzy',
+    src: './assets/lofi/lagujazz2.mp3',
+  },
+  {
+    id: 2,
+    name: 'Fiersa Besari - Waktu Yang Salah',
+    mood: 'jazzy',
+    src: './assets/lofi/lagujazz3.mp3',
+  },
+  {
+    id: 3,
+    name: 'RAISA - Could it Be',
+    mood: 'jazzy',
+    src: './assets/lofi/lagujazz4.mp3',
+  },
+  {
+    id: 4,
+    name: 'Noah - Walau Habis Terang',
+    mood: 'jazzy',
+    src: './assets/lofi/lagujazz5.mp3',
+  },
+  {
+    id: 5,
+    name: 'For Revenge - Jakarta Hari Ini',
+    mood: 'jazzy',
+    src: './assets/lofi/lagujazz6.mp3',
+  },
+  {
+    id: 6,
+    name: 'RAN - Dekat di Hati',
+    mood: 'jazzy',
+    src: './assets/lofi/lagujazz7.mp3',
+  },
+];
+
+export const sleep = [
+  {
+    id: 0,
+    name: 'Mood',
+    mood: 'sleep',
+    src: './assets/lofi/tidur1.mp3',
+  },
+  {
+    id: 1,
+    name: 'Glenn Fredly - Januari',
+    mood: 'sleep',
+    src: './assets/lofi/tidur2.mp3',
+  },
+  {
+    id: 2,
+    name: 'Drive - Bersama Bintang',
+    mood: 'sleep',
+    src: './assets/lofi/tidur3.mp3',
+  },
+  {
+    id: 3,
+    name: 'Somewhere only we know - Keane',
+    mood: 'sleep',
+    src: './assets/lofi/tidur4.mp3',
+  },
+  {
+    id: 4,
+    name: 'Nadin Amizah - Bertaut',
+    mood: 'sleep',
+    src: './assets/lofi/tidur5.mp3',
+  },
+  {
+    id: 5,
+    name: 'Mahalini - Bawa Dia Kembali',
+    mood: 'sleep',
+    src: './assets/lofi/tidur6.mp3',
+  },
+  {
+    id: 6,
+    name: 'Andmesh - Hanya Rindu',
+    mood: 'sleep',
+    src: './assets/lofi/tidur7.mp3',
+  },
+];
